6b09c8a62949fc2456ac2abf4dd8ad96c7ab1f9d
compiler <lukerochedev@gmail.com>
2021-02-01 10:50:48 -0500

    setup downloading and core

--- src/core.py
+++ src/core.py
@@ -1,12 +1,19 @@
 import gpt_2_simple as gpt2
 from datetime import datetime
-
-gpt2.finetune(  gpt2.start_tf_sess,
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+session = gpt2.start_tf_sess()
+gpt2.finetune(  session,
                 dataset="enc.npz",
-                model_name='124M',
-                steps=1000,
+                model_name='117M',
+                optimizer='sgd',
+                batch_size=1,
+                sample_length=256,
+                learning_rate=0.0001,
+                steps=200,
                 restore_from='fresh',
+
                 run_name='run1',
-                print_every=100,
+                print_every=10,
                 sample_every=100,
                 save_every=1000)
